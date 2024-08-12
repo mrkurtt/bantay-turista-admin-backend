209fb2c7ff27a506848dc429e7c5d3a5ce94eb89
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-08-12 23:36:06 +0800

    added routes in server.js

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ const app = express();
 
 // dependencies
 const authRoutes = require('./api/routes/auth.route');
+const adminRoutes = require('./api/routes/admin.route');
+const establishmentRoutes = require('./api/routes/establishment.route');
+const logRoutes = require('./api/routes/log.route');
+const touristRoutes = require('./api/routes/log.route');
 
 // connect to database
 connectToDb();
@@ -19,6 +23,11 @@ app.use(express.json());
 
 // routes
 app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/establishment', establishmentRoutes);
+app.use('/api/log', logRoutes);
+app.use('/api/tourist', touristRoutes);
+
 // start server
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
