996b6e132147a6d897054df90b9be66c4b39fe70
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-08-14 09:06:58 +0800

    feat: auth signup and login

---
+++ src/api/controllers/auth.controller.js
@@ -0,0 +1,78 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+
+const signUp = async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		// check if the user is already exists
+		const existingUser = await User.findOne({ username });
+		if (existingUser) {
+			return res.status(400).json({
+				success: false,
+				message: 'User already exists',
+			});
+		}
+
+		const hashedPassword = await bcrypt.hashSync(password, 10);
+
+		const newUser = new User({
+			username,
+			password: hashedPassword,
+		});
+
+		await newUser.save();
+
+		res.status(201).json({
+			success: true,
+			message: 'User signed up successfully',
+			userId: newUser._id,
+		});
+	} catch (error) {}
+};
+
+const login = async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		const user = await User.findOne({ username });
+		if (!user) {
+			return res.status(400).json({
+				success: false,
+				message: 'Invalid credentials',
+			});
+		}
+
+		// compare password
+		const isMatch = await bcrypt.compareSync(password, user.password);
+		if (!isMatch) {
+			return res.status(400).json({
+				success: false,
+				message: 'Invalid credentials',
+			});
+		}
+
+		// sign and create JWT
+		const token = jwt.sign(
+			{ userId: user._id, username: user.username },
+			process.env.JWT_SECRET,
+			{
+				expiresIn: '12h',
+			}
+		);
+
+		res.status(200).json({
+			success: true,
+			message: 'User successfully logged in',
+			token,
+		});
+	} catch (error) {
+		res.status(500).json({ message: 'Server error', error });
+	}
+};
+
+module.exports = {
+	signUp,
+	login,
+};
--- src/api/routes/auth.route.js
+++ src/api/routes/auth.route.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/auth.controller');
 
 router.get('/', async (req, res) => {
 	res.json({
-		msg: 'Hello',
+		msg: 'Auth route test',
 	});
 });
+
+router.post('/signup', authController.signUp);
+router.post('/login', authController.login);
 
 module.exports = router;
